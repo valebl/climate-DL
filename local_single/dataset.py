c3d972c4ba2e120e44f9ba6fe74a9b676d6db9b8
valebl <kiwi5.vb@gmail.com>
2023-02-23 17:33:53 +0100

    modify to add subgraph functionality

--- local_single/dataset.py
+++ local_single/dataset.py
@@ -10,15 +10,16 @@ from torch_geometric.data import Data
 
 class Dataset_pr(Dataset):
 
-    def __init__(self, args, pad=2, lat_dim=43, lon_dim=49, shift=2): #path, input_file, target_file, data_file, idx_file, net_type, get_key=False, mask_file=None, weights_file=None, **kwargs):
+    def __init__(self, args, pad=2, lat_dim=15, lon_dim=31, shift=2): #path, input_file, target_file, data_file, idx_file, net_type, get_key=False, mask_file=None, weights_file=None, **kwargs):
         super().__init__()
         self.pad = pad
-        self.lat_dim = lat_dim # number of points in the GRIPHO rectangle (0.25 grid)
-        self.lon_dim = lon_dim
-        self.space_idxs_dim = self.lat_dim * self.lon_dim
+        self.lat_low_res_dim = lat_dim # number of points in the GRIPHO rectangle (0.25 grid)
+        self.lon_low_res_dim = lon_dim
+        self.space_low_res_dim = self.lat_low_res_dim * self.lon_low_res_dim
         self.shift = shift # relative shift between GRIPHO and ERA5 (idx=0 in ERA5 corresponds to 2 in GRIPHO)
         self.args = args
         self.length = None
+        self.low_res_abs = None
 
     def _load_data_into_memory(self): # path, input_file, target_file, data_file, idx_file, net_type, mask_file, weights_file):
         raise NotImplementedError
@@ -41,22 +42,22 @@ class Dataset_pr_ae(Dataset_pr):
         self.length = len(idx_to_key)
         return input, idx_to_key
 
-    def __getitem__(self, idx, lat_shift=29, lon_shift=0):
+    def __getitem__(self, idx):
         k = self.idx_to_key[idx]   
-        time_idx = k // self.space_idxs_dim
-        space_idx = k % self.space_idxs_dim
-        lat_idx = space_idx // self.lon_dim - lat_shift
-        lon_idx = space_idx % self.lon_dim - lon_shift
+        time_idx = k // self.space_low_res_dim
+        space_idx = k % self.space_low_res_dim
+        lat_idx = space_idx // self.lon_low_res_dim
+        lon_idx = space_idx % self.lon_low_res_dim
         #-- derive input
         input = self.input[time_idx - 24 : time_idx+1, :, :, lat_idx - self.pad + 2 : lat_idx + self.pad + 4, lon_idx - self.pad + 2 : lon_idx + self.pad + 4]
         return input
 
 
-class Dataset_pr_reg(Dataset_pr):
+class Dataset_pr_gnn(Dataset_pr):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.input, self.idx_to_key, self.target, self.data, self.mask = self._load_data_into_memory()
+        self.input, self.idx_to_key, self.target, self.graph = self._load_data_into_memory()
     
     def _load_data_into_memory(self):
         with open(self.args.input_path + self.args.input_file, 'rb') as f:
@@ -65,34 +66,36 @@ class Dataset_pr_reg(Dataset_pr):
             idx_to_key = pickle.load(f)
         with open(self.args.input_path + self.args.target_file, 'rb') as f:
             target = pickle.load(f)        
-        with open(self.args.input_path + self.args.data_file, 'rb') as f:
-            data = pickle.load(f)
-        with open(self.args.input_path + self.args.mask_file, 'rb') as f:
-            mask = pickle.load(f)
+        with open(self.args.input_path + self.args.graph_file, 'rb') as f:
+            graph = pickle.load(f)
         self.length = len(idx_to_key)
-        return input, idx_to_key, target, data, mask
-    
-    def __getitem__(self, idx, lat_shift=29, lon_shift=0):
+        self.low_res_abs = abs(graph.low_res)
+        return input, idx_to_key, target, graph
+
+    def __getitem__(self, idx):
         k = self.idx_to_key[idx]   
-        time_idx = k // self.space_idxs_dim
-        space_idx = k % self.space_idxs_dim
-        lat_idx = space_idx // self.lon_dim - lat_shift
-        lon_idx = space_idx % self.lon_dim - lon_shift
+        time_idx = k // self.space_low_res_dim
+        space_idx = k % self.space_low_res_dim
+        lat_idx = space_idx // self.lon_low_res_dim
+        lon_idx = space_idx % self.lon_low_res_dim
         #-- derive input
         input = self.input[time_idx - 24 : time_idx+1, :, :, lat_idx - self.pad + 2 : lat_idx + self.pad + 4, lon_idx - self.pad + 2 : lon_idx + self.pad + 4]
         #-- derive gnn data
-        y = torch.tensor(self.target[k])
-        edge_index = torch.tensor(self.data[space_idx]['edge_index'])
-        x = torch.tensor(self.data[space_idx]['x'][:,2])
-        mask = torch.tensor(self.mask[k].astype(bool)) 
-        data = Data(x=x, edge_index=edge_index, y=y, mask=mask)
-        return input, data
+        selected_cells = torch.tensor([ii * self.lon_low_res_dim + jj for ii in range(lat_idx-1,lat_idx+2) for jj in range(lon_idx-1,lon_idx+2)])
+        mask_subgraph = torch.isin(self.low_res_abs, selected_cells)        # abs to consider all nodes (regarldess nan)
+        subgraph = self.graph.subgraph(subset=mask_subgraph)
+        subgraph["y"] = self.target[mask_subgraph][:,time_idx]                           # target is for a single time step
+        not_nan = ~torch.isnan(subgraph.y])                                 # retrieve mask of not nan target
+        mask_cell = torch.isin(subgraph.low_res, space_idx)                 # retrieve mask of central cell
+        subgraph["train_mask"] = torch.logical_and(not_nan, mask_cell)      # loss computed on nodes in central cell with not nan target
+        return input, subgraph
 
+'''
 class Dataset_pr_cl(Dataset_pr):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.input, self.idx_to_key, self.target, self.data = self._load_data_into_memory()
+        self.input, self.idx_to_key, self.target, self.graph = self._load_data_into_memory()
     
     def _load_data_into_memory(self):
         with open(self.args.input_path + self.args.input_file, 'rb') as f:
@@ -101,25 +104,31 @@ class Dataset_pr_cl(Dataset_pr):
             idx_to_key = pickle.load(f)
         with open(self.args.input_path + self.args.target_file, 'rb') as f:
             target = pickle.load(f)        
-        with open(self.args.input_path + self.args.data_file, 'rb') as f:
-            data = pickle.load(f)
+        with open(self.args.input_path + self.args.graph_file, 'rb') as f:
+            graph = pickle.load(f)
+        #graph["y"] = target
         self.length = len(idx_to_key)
-        return input, idx_to_key, target, data
+        return input, idx_to_key, target, graph
  
-    def __getitem__(self, idx, lat_shift=29, lon_shift=0):
+    def __getitem__(self, idx):
         k = self.idx_to_key[idx]   
-        time_idx = k // self.space_idxs_dim
-        space_idx = k % self.space_idxs_dim
-        lat_idx = space_idx // self.lon_dim - lat_shift
-        lon_idx = space_idx % self.lon_dim - lon_shift
+        time_idx = k // self.space_low_res_dim
+        space_idx = k % self.space_low_res_dim
+        lat_idx = space_idx // self.lon_low_res_dim
+        lon_idx = space_idx % self.lon_low_res_dim
         #-- derive input
         input = self.input[time_idx - 24 : time_idx+1, :, :, lat_idx - self.pad + 2 : lat_idx + self.pad + 4, lon_idx - self.pad + 2 : lon_idx + self.pad + 4]
         #-- derive gnn data
-        y = torch.tensor(self.target[k])
-        edge_index = torch.tensor(self.data[space_idx]['edge_index'])
-        x = torch.tensor(self.data[space_idx]['x'][:,2])
-        data = Data(x=x, edge_index=edge_index, y=y)
-        return input, data
+        selected_cells = torch.tensor([ii * self.lon_low_res_dim + jj for ii in range(lat_idx-1,lat_idx+2) for jj in range(lon_idx-1,lon_idx+2)])
+        mask_subgraph = torch.isin(self.low_res_abs, selected_cells)        # abs to consider all nodes (regarldess nan)
+        subgraph = self.graph.subgraph(subset=mask_subgraph)
+        subgraph.y = subgraph.y[:,time_idx]                           # target is for a single time step
+        not_nan = ~torch.isnan(subgraph.y)                      # retrieve mask of not nan target
+        mask_cell = torch.isin(subgraph.low_res, space_idx)                 # retrieve mask of central cell
+        subgraph["train_mask"] = torch.logical_and(not_nan, mask_cell)      # loss computed on nodes in central cell with not nan target
+        subgraph.y[train_mask] = torch.where(subgraph.y[train_mask])
+        return input, subgraph
+'''
 
 def custom_collate_fn_ae(batch):
     input = np.array(batch)
