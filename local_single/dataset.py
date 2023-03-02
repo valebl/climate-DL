bc89fa6ae7e26d438cd2684dae0ffe295bc49ffd
valebl <kiwi5.vb@gmail.com>
2023-03-02 15:31:14 +0100

    fix subgraph and masks in dataset

--- local_single/dataset.py
+++ local_single/dataset.py
@@ -57,7 +57,8 @@ class Dataset_pr_gnn(Dataset_pr):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.input, self.idx_to_key, self.target, self.graph = self._load_data_into_memory()
+        self.input, self.idx_to_key, self.target, self.graph, self.mask_target,
+            self.mask_1_cell, self.mask_9_cells = self._load_data_into_memory()
     
     def _load_data_into_memory(self):
         with open(self.args.input_path + self.args.input_file, 'rb') as f:
@@ -68,9 +69,15 @@ class Dataset_pr_gnn(Dataset_pr):
             target = pickle.load(f)        
         with open(self.args.input_path + self.args.graph_file, 'rb') as f:
             graph = pickle.load(f)
+        with open(self.args.input_path + self.args.mask_target_file, 'rb') as f:
+            mask_target = pickle.load(f)
+        with open(self.args.input_path + self.args.mask_1_cell_file, 'rb') as f:
+            mask_1_cell = pickle.load(f)
+        with open(self.args.input_path + self.args.mask_9_cells_file, 'rb') as f:
+            mask_9_cells = pickle.load(f)
         self.length = len(idx_to_key)
         self.low_res_abs = abs(graph.low_res)
-        return input, idx_to_key, target, graph
+        return input, idx_to_key, target, graph, mask_target, mask_1_cell, mask_9_cells
 
     def __getitem__(self, idx):
         k = self.idx_to_key[idx]   
@@ -81,14 +88,21 @@ class Dataset_pr_gnn(Dataset_pr):
         #-- derive input
         input = self.input[time_idx - 24 : time_idx+1, :, :, lat_idx - self.pad + 2 : lat_idx + self.pad + 4, lon_idx - self.pad + 2 : lon_idx + self.pad + 4]
         #-- derive gnn data
-        selected_cells = torch.tensor([ii * self.lon_low_res_dim + jj for ii in range(lat_idx-1,lat_idx+2) for jj in range(lon_idx-1,lon_idx+2)])
-        mask_subgraph = torch.isin(self.low_res_abs, selected_cells)        # abs to consider all nodes (regarldess nan)
+        mask_subgraph = self.mask_9_cells[space_idx] # shape = (n_nodes,)
         subgraph = self.graph.subgraph(subset=mask_subgraph)
-        subgraph["y"] = self.target[mask_subgraph][:,time_idx]                           # target is for a single time step
-        not_nan = ~torch.isnan(subgraph.y])                                 # retrieve mask of not nan target
-        mask_cell = torch.isin(subgraph.low_res, space_idx)                 # retrieve mask of central cell
-        subgraph["train_mask"] = torch.logical_and(not_nan, mask_cell)      # loss computed on nodes in central cell with not nan target
+        mask_y_nodes = self.mask_1_cell[space_idx] * self.mask_target[:,time_idx] # shape = (n_nodes,)
+        subgraph["train_mask"] = mask_y_nodes[mask_subgraph]
+        y = self.target[mask_y_nodes, time_idx] # shape = (n_nodes_train,)
+        subgraph["y"] = y
         return input, subgraph
+        #selected_cells = torch.tensor([ii * self.lon_low_res_dim + jj for ii in range(lat_idx-1,lat_idx+2) for jj in range(lon_idx-1,lon_idx+2)])
+        #mask_subgraph = torch.isin(self.low_res_abs, selected_cells)        # abs to consider all nodes (regarldess nan)
+        #subgraph = self.graph.subgraph(subset=mask_subgraph)
+        #subgraph["y"] = self.target[mask_subgraph][:,time_idx]                           # target is for a single time step
+        #not_nan = ~torch.isnan(subgraph.y])                                 # retrieve mask of not nan target
+        #mask_cell = torch.isin(subgraph.low_res, space_idx)                 # retrieve mask of central cell
+        #subgraph["train_mask"] = torch.logical_and(not_nan, mask_cell)      # loss computed on nodes in central cell with not nan target
+
 
 '''
 class Dataset_pr_cl(Dataset_pr):
