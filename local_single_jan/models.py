f03e68731436022c9876ca9d7da0a32d98e39f2b
valebl <valebl@DESKTOP-06R2CFC.localdomain>
2023-05-22 10:10:04 +0200

    add directory with working code of january

---
+++ local_single_jan/models.py
@@ -0,0 +1,275 @@
+import numpy as np
+import torch
+from torch import nn
+from torch_geometric import nn as geometric_nn
+from torch_geometric.nn import GATConv, GATv2Conv
+from torch_geometric.data import Batch
+import sys
+
+class Autoencoder(nn.Module):
+    def __init__(self, input_size=5, input_dim=256, hidden_dim=256, output_dim=256, n_layers=2):
+        super().__init__() 
+        self.output_dim = output_dim
+
+        self.encoder = nn.Sequential(
+            nn.Conv3d(input_size, 64, kernel_size=3, padding=(1,1,1), stride=1),
+            nn.BatchNorm3d(64),
+            nn.ReLU(),
+            nn.Conv3d(64, 64, kernel_size=3, padding=(1,1,1), stride=1),
+            nn.BatchNorm3d(64),
+            nn.ReLU(),
+            nn.MaxPool3d(kernel_size=2, padding=(1,1,1), stride=2),   
+            nn.Conv3d(64, 256, kernel_size=3, padding=(1,1,1), stride=1),
+            nn.BatchNorm3d(256),
+            nn.ReLU(),
+            nn.MaxPool3d(kernel_size=2, padding=(1,0,0), stride=2),   
+            nn.Flatten(),
+            nn.Linear(2048, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Linear(512, output_dim),
+            nn.BatchNorm1d(output_dim),
+            nn.ReLU()            
+            )   
+
+        self.gru = nn.Sequential(
+            nn.GRU(input_dim, hidden_dim, n_layers, batch_first=True),        
+            )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(output_dim, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Linear(512, 2048),
+            nn.Unflatten(-1,(256, 2, 2, 2)),
+            nn.Upsample(size=(3,4,4)),
+            nn.ReLU(),
+            nn.ConvTranspose3d(256, 64, kernel_size=3, padding=(1,1,1), stride=1),
+            nn.ReLU(),
+            nn.Upsample(size=(5,6,6)),
+            nn.ReLU(),
+            nn.ConvTranspose3d(64, 64, kernel_size=3, padding=(1,1,1), stride=1),
+            nn.ReLU(),
+            nn.ConvTranspose3d(64, 5, kernel_size=3, padding=(1,1,1), stride=1),
+            )
+
+    def forward(self, X):
+        s = X.shape
+        X = X.reshape(s[0]*s[1], s[2], s[3], s[4], s[5])
+        X = self.encoder(X)
+        X = X.reshape(s[0], s[1], self.output_dim)
+        out, h = self.gru(X)
+        out = out.reshape(s[0]*s[1], self.output_dim)
+        out = self.decoder(out)
+        out = out.reshape(s[0], s[1], s[2], s[3], s[4], s[5])
+        return out
+
+
+class Classifier(nn.Module):
+    def __init__(self, input_size=5, input_dim=256, hidden_dim=256, output_dim=256, n_layers=2):
+        super().__init__()
+        self.output_dim = output_dim
+
+        self.encoder = nn.Sequential(
+            nn.Conv3d(input_size, 64, kernel_size=3, padding=(1,1,1), stride=1),
+            nn.BatchNorm3d(64),
+            nn.ReLU(),
+            nn.Conv3d(64, 64, kernel_size=3, padding=(1,1,1), stride=1),
+            nn.BatchNorm3d(64),
+            nn.ReLU(),
+            nn.MaxPool3d(kernel_size=2, padding=(1,1,1), stride=2),
+            nn.Conv3d(64, 256, kernel_size=3, padding=(1,1,1), stride=1),
+            nn.BatchNorm3d(256),
+            nn.ReLU(),
+            nn.MaxPool3d(kernel_size=2, padding=(1,0,0), stride=2),
+            nn.Flatten(),
+            nn.Linear(2048, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Linear(512, output_dim),
+            nn.BatchNorm1d(output_dim),
+            nn.ReLU()
+            )
+
+        # define the decoder modules
+        self.gru = nn.Sequential(
+            nn.GRU(input_dim, hidden_dim, n_layers, batch_first=True),
+        )
+
+        self.dense = nn.Sequential(
+            nn.Linear(hidden_dim*25, 512),
+            nn.ReLU()
+        )
+
+        #gnn
+        self.gnn = geometric_nn.Sequential('x, edge_index', [
+            (geometric_nn.BatchNorm(3+512), 'x -> x'),
+            (GATv2Conv(3+512, 128, heads=2, aggr='mean', dropout=0.5),  'x, edge_index -> x'),
+            (geometric_nn.BatchNorm(256), 'x -> x'),
+            nn.ReLU(),
+            (GATv2Conv(256, 128, aggr='mean'), 'x, edge_index -> x'),
+            (geometric_nn.BatchNorm(128), 'x -> x'),
+            nn.ReLU(),
+            (GATv2Conv(128, 1, aggr='mean'), 'x, edge_index -> x'), # focal loss
+            nn.Sigmoid()                                            # focal loss
+            ])
+
+    def forward(self, X_batch, data_batch, device):
+        s = X_batch.shape
+        X_batch = X_batch.reshape(s[0]*s[1], s[2], s[3], s[4], s[5])
+        X_batch = self.encoder(X_batch.to(device))
+        X_batch = X_batch.reshape(s[0], s[1], self.output_dim)
+        encoding, _ = self.gru(X_batch)
+        encoding = encoding.reshape(s[0], s[1]*self.output_dim)
+        encoding = self.dense(encoding)
+            
+        for i, data in enumerate(data_batch):
+            data = data.to(device)
+            features = torch.zeros((data.num_nodes, 3 + encoding.shape[1])).to(device)
+            features[:,:3] = data.x[:,:3]
+            features[:,3:] = encoding[i,:]
+            data.__setitem__('x', features)
+        data_batch = Batch.from_data_list(data_batch)
+        y_pred = self.gnn(data_batch.x, data_batch.edge_index)
+        return y_pred, data_batch.y.squeeze().to(torch.long), data_batch.batch
+
+
+class Regressor(nn.Module):
+    def __init__(self, input_size=5, input_dim=256, hidden_dim=256, output_dim=256, n_layers=2):
+        super().__init__()
+        self.output_dim = output_dim
+        self.encoder = nn.Sequential(
+            nn.Conv3d(input_size, 64, kernel_size=3, padding=(1,1,1), stride=1),
+            nn.BatchNorm3d(64),
+            nn.ReLU(),
+            nn.Conv3d(64, 64, kernel_size=3, padding=(1,1,1), stride=1),
+            nn.BatchNorm3d(64),
+            nn.ReLU(),
+            nn.MaxPool3d(kernel_size=2, padding=(1,1,1), stride=2),
+            nn.Conv3d(64, 256, kernel_size=3, padding=(1,1,1), stride=1),
+            nn.BatchNorm3d(256),
+            nn.ReLU(),
+            nn.MaxPool3d(kernel_size=2, padding=(1,0,0), stride=2),
+            nn.Flatten(),
+            nn.Linear(2048, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Linear(512, output_dim),
+            nn.BatchNorm1d(output_dim),
+            nn.ReLU()
+            )
+
+        self.gru = nn.Sequential(
+            nn.GRU(input_dim, hidden_dim, n_layers, batch_first=True),
+        )
+
+        self.dense = nn.Sequential(
+            nn.Linear(hidden_dim*25, 512),
+            nn.ReLU()
+        )
+
+        self.gnn = geometric_nn.Sequential('x, edge_index', [
+            (geometric_nn.BatchNorm(3+512), 'x -> x'),
+            (GATv2Conv(3+512, 128, heads=2, aggr='mean', dropout=0.5),  'x, edge_index -> x'), 
+            (geometric_nn.BatchNorm(256), 'x -> x'),
+            nn.ReLU(),
+            (GATv2Conv(256, 128, aggr='mean'), 'x, edge_index -> x'),
+            (geometric_nn.BatchNorm(128), 'x -> x'),
+            nn.ReLU(),
+            (GATv2Conv(128, 1, aggr='mean'), 'x, edge_index -> x'),
+            ])
+
+    def forward(self, X_batch, data_batch, device):
+        s = X_batch.shape
+        X_batch = X_batch.reshape(s[0]*s[1], s[2], s[3], s[4], s[5])
+        X_batch = self.encoder(X_batch.to(device))
+        X_batch = X_batch.reshape(s[0], s[1], self.output_dim)
+        encoding, _ = self.gru(X_batch)
+        encoding = encoding.reshape(s[0], s[1]*self.output_dim)
+        encoding = self.dense(encoding)
+            
+        for i, data in enumerate(data_batch):
+            data = data.to(device)
+            features = torch.zeros((data.num_nodes, 3 + encoding.shape[1])).to(device)
+            features[:,:3] = data.x[:,:3]
+            features[:,3:] = encoding[i,:]
+            data.__setitem__('x', features)
+        data_batch = Batch.from_data_list(data_batch)
+        y_pred = self.gnn(data_batch.x, data_batch.edge_index)
+        mask = data_batch.mask.squeeze()
+        return y_pred.squeeze()[mask], data_batch.y.squeeze()[mask], data_batch.batch[mask]
+
+
+class Classifier_test(Classifier):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, X_batch, data_batch, device):
+        s = X_batch.shape
+        X_batch = X_batch.reshape(s[0]*s[1], s[2], s[3], s[4], s[5])
+        X_batch = self.encoder(X_batch.to(device))
+        X_batch = X_batch.reshape(s[0], s[1], self.output_dim)
+        encoding, _ = self.gru(X_batch)
+        encoding = encoding.reshape(s[0], s[1]*self.output_dim)
+        encoding = self.dense(encoding)
+
+        for i, data in enumerate(data_batch):
+            data = data.to(device)
+            features = torch.zeros((data.num_nodes, 3 + encoding.shape[1])).to(device)
+            features[:,:3] = data.x[:,:3]
+            features[:,3:] = encoding[i,:]
+            data.__setitem__('x', features)
+        data_batch = Batch.from_data_list(data_batch)
+        y_pred = self.gnn(data_batch.x, data_batch.edge_index)
+        prediction_class = torch.where(y_pred >= 0.5, 1, 0)              # focal loss 
+        y = data_batch.y
+        return prediction_class, y, data_batch.batch                             # focal loss
+
+
+class Regressor_test(Regressor):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, X_batch, data_batch, device):
+        s = X_batch.shape
+        X_batch = X_batch.reshape(s[0]*s[1], s[2], s[3], s[4], s[5])
+        X_batch = self.encoder(X_batch.to(device))
+        X_batch = X_batch.reshape(s[0], s[1], self.output_dim)
+        encoding, _ = self.gru(X_batch)
+        encoding = encoding.reshape(s[0], s[1]*self.output_dim)
+        encoding = self.dense(encoding)
+
+        for i, data in enumerate(data_batch):
+            data = data.to(device)
+            features = torch.zeros((data.num_nodes, 3 + encoding.shape[1])).to(device)
+            features[:,:3] = data.x[:,:3]
+            features[:,3:] = encoding[i,:]
+            data.__setitem__('x', features)
+        data_batch = Batch.from_data_list(data_batch)
+        y_pred = self.gnn(data_batch.x, data_batch.edge_index)
+        y_pred = torch.expm1(y_pred)                            # log1p
+        y = data_batch.y
+        return y_pred, y, data_batch.batch
+
+
+class Regressor_test_large(Regressor):
+    
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, data_batch):
+        y_pred = torch.expm1(self.gnn(data_batch.x, data_batch.edge_index))
+        return y_pred
+
+
+class Classifier_test_large(Classifier):
+    
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, data_batch):
+        y_pred = self.gnn(data_batch.x, data_batch.edge_index)
+        y_pred = torch.where(y_pred > 0.5, 1, 0) # focal loss
+        return y_pred
---
+++ local_single_jan/utils.py
@@ -0,0 +1,314 @@
+import numpy as np
+import time
+import sys
+import pickle
+
+import torch
+
+#------Some useful utilities------
+
+class AverageMeter(object):
+    '''
+    a generic class to keep track of performance metrics during training or testing of models
+    (from the Deep Learning tutorials of DSSC)
+    '''
+    def __init__(self):
+        self.reset()
+        self.avg_list = []
+        self.avg_iter_list = []
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+    
+    def add_loss(self):
+        self.avg_list.append(self.avg)
+    
+    def add_iter_loss(self):
+        self.avg_iter_list.append(self.avg)
+
+
+def use_gpu_if_possible():
+    return "cuda:0" if torch.cuda.is_available() else "cpu"
+
+
+def accuracy(prediction, target):
+    if prediction.shape == target.shape:
+        prediction_class = torch.where(prediction > 0.5, 1.0, 0.0) 
+    else:
+        prediction_class = torch.argmax(prediction, dim=-1).squeeze()
+    correct_items = (prediction_class == target)
+    acc = correct_items.sum().item() / prediction.shape[0]  
+    return acc
+
+
+def weighted_mse_loss(input_batch, target_batch, weights):
+    return (weights * (input_batch - target_batch) ** 2).sum() / weights.sum()
+
+
+def load_encoder_checkpoint(model, checkpoint, log_path, log_file, accelerator, fine_tuning=True, net_names=['encoder', 'gru', 'linear']):
+    if accelerator is None or accelerator.is_main_process:
+        with open(log_path+log_file, 'a') as f:
+            f.write("\nLoading encoder parameters.") 
+    checkpoint = torch.load(checkpoint)
+    state_dict = checkpoint["parameters"]
+    for name, param in state_dict.items():
+        for net_name in net_names:
+            if net_name in name:
+                if accelerator is None or accelerator.is_main_process:
+                    with open(log_path+log_file, 'a') as f:
+                        f.write(f"\nLoading parameters '{name}'")
+                param = param.data
+                if name.startswith("module"):
+                    name = name.partition("module.")[2]
+                try:
+                    model.state_dict()[name].copy_(param)
+                except:
+                     if accelerator is None or accelerator.is_main_process:
+                        with open(log_path+log_file, 'a') as f:
+                            f.write(f"\nParam {name} was not loaded..")
+    if not fine_tuning:
+        for net_name in net_names:
+            [param.requires_grad_(False) for name, param in model.named_parameters() if net_name in name]
+    return model
+
+
+def check_freezed_layers(model, log_path, log_file, accelerator):
+    for name, param in model.named_parameters():
+        n_param = param.numel()
+        if accelerator is None or accelerator.is_main_process:
+            with open(log_path+log_file, 'a') as f:
+                f.write(f"\nLayer {name} requires_grad = {param.requires_grad} and has {n_param} parameters")
+    return
+
+
+def countour_italy_list(path='/home/vblasone/precipitation-maps/', file_name='Italia.txt'):
+    zones = []
+    with open('/home/vblasone/precipitation-maps/Italia.txt') as f:
+        lines = f.read()
+        for zone in lines.split(';'):
+            zones.append(zone)
+    for i in range(len(zones)):
+        zones[i] = zones[i].split('\n')
+        for j in range(len(zones[i])):
+            zones[i][j] = zones[i][j].split(',')
+        if [''] in zones[i]:
+            zones[i].remove([''])
+    for i in range(len(zones)):
+        for j in range(len(zones[i])):
+            if '' in zones[i][j]:
+                zones[i][j].remove('')
+            if zones[i][j] == []:
+                del zones[i][j]
+                continue
+            for k in range(len(zones[i][j])):
+                zones[i][j][k] = float(zones[i][j][k])
+    return zones
+
+
+def draw_rectangle(x_min, x_max, y_min, y_max, color, ax, fill=False, fill_color=None, alpha=0.5):
+    y_grid = [y_min, y_min, y_max, y_max, y_min]
+    x_grid = [x_min, x_max, x_max, x_min, x_min]
+    ax.plot(x_grid, y_grid, color=color)
+    if fill:
+        if fill_color==None:
+            fill_color = color
+        ax.fill(x_grid, y_grid, color=fill_color, alpha=alpha)
+    return
+
+
+def plot_countour_italy(zones, ax, color='k', alpha_fill=0.1, linewidth=1):
+    j = 0
+    for zone in zones:
+        x_zone = [zone[i][0] for i in range(len(zone)) if i > 0]
+        y_zone = [zone[i][1] for i in range(len(zone)) if i > 0]
+        ax.fill(x_zone, y_zone, color, alpha=alpha_fill)
+        ax.plot(x_zone, y_zone, color, alpha=1, linewidth=1)
+        j += 1
+    return
+
+
+#------Training utilities------
+
+#------EPOCH LOOPS------  
+
+def train_epoch_ae(model, dataloader, loss_fn, optimizer, lr_scheduler, loss_meter, performance_meter, val_loss_meter,
+        val_performance_meter, log_path, log_file, validationloader, validate_model, accelerator, intermediate=False, epoch=0):
+    
+    loss_meter.reset()
+    val_loss_meter.reset()
+    if performance_meter is not None:
+        performance_meter.reset()
+        val_performance_meter.reset()
+
+    i = 0
+    for X in dataloader:
+        if accelerator is None:
+            X = X.cuda()
+        optimizer.zero_grad()
+        X_pred = model(X)
+        loss = loss_fn(X_pred, X)
+        if accelerator is None:
+            loss.backward()
+        else:
+            accelerator.backward(loss)
+        optimizer.step()
+        loss_meter.update(val=loss.item(), n=X.shape[0])
+        loss_meter.add_iter_loss()
+ 
+        if performance_meter is not None:
+            perf = accuracy(X_pred, X)
+            performance_meter.update(val=perf, n=X.shape[0])
+            performance_meter.add_iter_loss()
+
+        if i % 5000 == 0:
+            validate_model(model, validationloader, accelerator, loss_fn, val_loss_meter, val_performance_meter)
+            if intermediate:
+                with open(log_path+log_file, 'a') as f:
+                    if val_performance_meter is not None:
+                        f.write(f"\nValidation loss at iteration {i}, tot = {val_loss_meter.sum}, avg = {val_loss_meter.avg}, val perf avg = {val_performance_meter.avg}.")
+                    else:
+                        f.write(f"\nValidation loss at iteration {i}, tot = {val_loss_meter.sum}, avg = {val_loss_meter.avg}")
+            np.savetxt(log_path+"val_loss_iter.csv", val_loss_meter.avg_iter_list)
+            np.savetxt(log_path+"train_loss_iter.csv", loss_meter.avg_iter_list)
+
+            if performance_meter is not None:
+                np.savetxt(log_path+"train_accuracy_iter.csv", performance_meter.avg_iter_list)
+                np.savetxt(log_path+"val_accuracy_iter.csv", val_performance_meter.avg_iter_list)
+        i += 1
+
+    validate_model(model, validationloader, accelerator, loss_fn, val_loss_meter, val_performance_meter)
+
+
+def train_epoch_gnn(model, dataloader, loss_fn, optimizer, lr_scheduler, loss_meter, performance_meter, val_loss_meter,
+            val_performance_meter, log_path, log_file, validationloader, validate_model, accelerator, intermediate=False, epoch=0):
+
+    loss_meter.reset()
+    val_loss_meter.reset()
+    if performance_meter is not None:
+        performance_meter.reset()
+        val_performance_meter.reset()
+
+    model.train()
+    i = 0
+    for X, data in dataloader:
+        device = 'cuda' if accelerator is None else accelerator.device
+        optimizer.zero_grad()
+        y_pred, y, _  = model(X, data, device)
+        loss = loss_fn(y_pred, y)
+        if accelerator is None:
+            loss.backward()
+        else:
+            accelerator.backward(loss)
+        torch.nn.utils.clip_grad_norm_(model.parameters(),5)
+        optimizer.step()
+        loss_meter.update(val=loss.item(), n=X.shape[0])    
+        loss_meter.add_iter_loss()    
+
+        if performance_meter is not None:
+            perf = accuracy(y_pred, y)
+            performance_meter.update(val=perf, n=X.shape[0])
+            performance_meter.add_iter_loss()
+
+        if i % 5000 == 0:
+            validate_model(model, validationloader, accelerator, loss_fn, val_loss_meter, val_performance_meter)
+            if intermediate:
+                with open(log_path+log_file, 'a') as f:
+                    if val_performance_meter is not None:
+                        f.write(f"\nValidation loss at iteration {i}, tot = {val_loss_meter.sum}, avg = {val_loss_meter.avg}, val perf avg = {val_performance_meter.avg}.")
+                    else:
+                        f.write(f"\nValidation loss at iteration {i}, tot = {val_loss_meter.sum}, avg = {val_loss_meter.avg}")
+        
+            if accelerator is None or accelerator.is_main_process:
+                np.savetxt(log_path+"train_loss_iter.csv", loss_meter.avg_iter_list)
+                np.savetxt(log_path+"val_loss_iter.csv", val_loss_meter.avg_iter_list)
+                if performance_meter is not None:
+                    np.savetxt(log_path+"train_perf_iter.csv", performance_meter.avg_iter_list)
+                    np.savetxt(log_path+"val_perf_iter.csv", val_performance_meter.avg_iter_list)
+                checkpoint_dict = {
+                    "parameters": model.state_dict(),
+                    "optimizer": optimizer.state_dict(),
+                    "epoch": epoch
+                    }
+                torch.save(checkpoint_dict, log_path+"checkpoint.pth")
+
+        i += 1
+
+    validate_model(model, validationloader, accelerator, loss_fn, val_loss_meter, val_performance_meter)
+
+
+#------ TRAIN ------  
+
+def train_model(model, dataloader, loss_fn, optimizer, num_epochs,
+        log_path, log_file, train_epoch, validate_model, validationloader, accelerator,
+        lr_scheduler=None, checkpoint_name="checkpoint.pth", performance=None, epoch_start=0):
+    
+    model.train()
+
+    # define average meter objects
+    loss_meter = AverageMeter()
+    val_loss_meter = AverageMeter()
+
+    if performance is not None:
+        performance_meter = AverageMeter()
+        val_performance_meter = AverageMeter()
+    else:
+        performance_meter = None
+        val_performance_meter = None
+
+    # epoch loop
+    for epoch in range(epoch_start, epoch_start + num_epochs):
+        
+        if accelerator is None or accelerator.is_main_process:
+            with open(log_path+log_file, 'a') as f:
+                f.write(f"\nEpoch {epoch+1} --- learning rate {optimizer.param_groups[0]['lr']:.8f}")
+        
+        start_time = time.time()
+        
+        train_epoch(model, dataloader, loss_fn, optimizer, lr_scheduler, loss_meter, performance_meter, val_loss_meter,
+            val_performance_meter, log_path, log_file, validationloader, validate_model, accelerator, epoch=epoch)
+        
+        end_time = time.time()
+        
+        loss_meter.add_loss()
+        val_loss_meter.add_loss()
+        if performance is not None:
+            performance_meter.add_loss()
+            val_performance_meter.add_loss()
+
+        if lr_scheduler is not None and lr_scheduler.get_last_lr()[0] > 0.000001:
+            lr_scheduler.step()
+
+        if accelerator is None or accelerator.is_main_process:
+            with open(log_path+log_file, 'a') as f:
+                if performance_meter is None:
+                    f.write(f"\nEpoch {epoch+1} completed in {end_time - start_time:.4f} seconds. Loss - total: {loss_meter.sum:.4f} - average: {loss_meter.avg:.10f}. "+
+                            f"Validation loss avg = {val_loss_meter.avg:.4f}")
+                else:
+                    f.write(f"\nEpoch {epoch+1} completed in {end_time - start_time:.4f} seconds. Loss - total: {loss_meter.sum:.4f} - average: {loss_meter.avg:.10f}; "+
+                            f"performance: {performance_meter.avg:.4f}. Validation loss avg = {val_loss_meter.avg:.4f}; performance: {val_performance_meter.avg:.4f}")
+
+            np.savetxt(log_path+"train_loss.csv", loss_meter.avg_list)
+            np.savetxt(log_path+"val_loss.csv", val_loss_meter.avg_list)
+            np.savetxt(log_path+"train_loss_iter.csv", loss_meter.avg_iter_list)
+            np.savetxt(log_path+"val_loss_iter.csv", val_loss_meter.avg_iter_list)
+            if performance is not None:
+                np.savetxt(log_path+"train_perf.csv", performance_meter.avg_list)
+                np.savetxt(log_path+"val_perf.csv", val_performance_meter.avg_list)
+                np.savetxt(log_path+"train_perf_iter.csv", performance_meter.avg_iter_list)
+                np.savetxt(log_path+"val_perf_iter.csv", val_performance_meter.avg_iter_list)
+            checkpoint_dict = {
+                "parameters": model.state_dict(),
+                "optimizer": optimizer.state_dict(),
+                "epoch": epoch,
+                }
+            torch.save(checkpoint_dict, checkpoint_name)
+
