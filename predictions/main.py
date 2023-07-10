a0cca3469fc082d34ca60dd1847da1112d62fef0
valebl <valebl@DESKTOP-06R2CFC.localdomain>
2023-07-10 11:37:06 +0200

    fix plots

--- predictions/main.py
+++ predictions/main.py
@@ -138,10 +138,14 @@ if __name__ == '__main__':
 #----------------------- PLOTS -----------------------
 #-----------------------------------------------------
 
+    x_size = args.lon_dim
+    y_size = args.lat_dim
+    font_size = int(12 / 7 * x_size)
+
     if args.make_plots:        
         with open(args.output_path + args.log_file, 'a') as f:
             f.write(f"\n\nMaking some plots.")
-        with open("/m100_work/ICT23_ESP_C/vblasone/climate-DL/data_fvg_preprocessed/valid_examples_space.pkl", 'rb') as f:
+        with open(args.input_path+"valid_examples_space.pkl", 'rb') as f:
             valid_examples_space = pickle.load(f)
         zones = create_zones()
         mask = np.logical_and(np.array([~torch.isnan(G_test.y[i,:]).all().numpy() for i in range(G_test.pr.shape[0])]), np.in1d(G_test.low_res, valid_examples_space))
@@ -153,22 +157,24 @@ if __name__ == '__main__':
         acc_1 = corrects[y_cl.flatten()==1].sum() / len(y_cl.flatten()[y_cl.flatten()==1]) * 100            
         with open(args.output_path + args.log_file, 'a') as f:
             f.write(f"\nClassifier\nAccuracy: {acc:.2f}\nAccuracy on class 0: {acc_0:.2f}\nAccuracy on class 1: {acc_1:.2f}")
-        plot_maps(G_test.pos[mask,:], G_test.pr_cl.numpy()[mask,:], y_cl.numpy(), pr_min=0.1, pr_max=2000, aggr=np.sum,
+        
+        plot_maps(G_test.pos[mask,:], G_test.pr_cl.numpy()[mask,:], y_cl.numpy(), pr_min=0.1, pr_max=2000, aggr=np.nansum,
             title=f"Classifier - Number of hours with pr>=0.1mm for the year {args.test_year}", idx_start=0, idx_end=-1, legend_title="hours",
-            save_path=args.output_path, save_file_name=f"maps_cl_{args.test_year}.{args.img_extension}", zones=zones)
+            save_path=args.output_path, save_file_name=f"maps_cl_{args.test_year}.{args.img_extension}", zones=zones, x_size=x_size, y_size=y_size, font_size=font_size)
         # Regressor
         pr_mask = G_test.y.numpy()[mask,:] >= 0.1
-        plot_maps(G_test.pos[mask,:], G_test.pr_reg.numpy()[mask,:] * pr_mask, G_test.y.numpy()[mask,:] * pr_mask, pr_min=0.1, pr_max=2500, aggr=np.sum,
+        plot_maps(G_test.pos[mask,:], G_test.pr_reg.numpy()[mask,:] * pr_mask, G_test.y.numpy()[mask,:] * pr_mask, pr_min=0.1, pr_max=2500, aggr=np.nansum,
             title=f"Regressor - Cumulative precipitation when pr>=0.1 in observations for the year {args.test_year}", idx_start=0, idx_end=-1,
-            save_path=args.output_path, save_file_name=f"maps_reg_{args.test_year}.{args.img_extension}", zones=zones)
+            save_path=args.output_path, save_file_name=f"maps_reg_{args.test_year}.{args.img_extension}", zones=zones, x_size=x_size, y_size=y_size, font_size=font_size)
         # Combines results
-        plot_maps(G_test.pos[mask,:], G_test.pr.numpy()[mask,:], G_test.y.numpy()[mask,:], pr_min=0.1, pr_max=2500, aggr=np.sum,
+        plot_maps(G_test.pos[mask,:], G_test.pr.numpy()[mask,:], G_test.y.numpy()[mask,:], pr_min=0.1, pr_max=2500, aggr=np.nansum,
             title=f"Cumulative precipitation for the year {args.test_year}", idx_start=0, idx_end=-1,
-            save_path=args.output_path, save_file_name=f"maps_cumulative_{args.test_year}.{args.img_extension}", zones=zones)
-        plot_maps(G_test.pos[mask,:], G_test.pr.numpy()[mask,:], G_test.y.numpy()[mask,:], pr_min=0.0, pr_max=0.25, aggr=np.mean,
+            save_path=args.output_path, save_file_name=f"maps_cumulative_{args.test_year}.{args.img_extension}", zones=zones, x_size=x_size, y_size=y_size, font_size=font_size)
+        plot_maps(G_test.pos[mask,:], G_test.pr.numpy()[mask,:], G_test.y.numpy()[mask,:], pr_min=0.0, pr_max=0.25, aggr=np.nanmean,
             title=f"Mean precipitation for the year {args.test_year}", idx_start=0, idx_end=-1,
-            save_path=args.output_path, save_file_name=f"maps_mean_{args.test_year}.{args.img_extension}", zones=zones)
+            save_path=args.output_path, save_file_name=f"maps_mean_{args.test_year}.{args.img_extension}", zones=zones, x_size=x_size, y_size=y_size, font_size=font_size)
         # Histogram
+        plt.rcParams.update({'font.size': 16})
         y = G_test.y.numpy()[mask,:].flatten()
         pr = G_test.pr.numpy()[mask,:].flatten()
         pr_reg = G_test.pr_reg[mask,:].numpy().flatten()
@@ -184,7 +190,7 @@ if __name__ == '__main__':
         plt.legend()
         plt.savefig(f'{args.output_path}histogram.{args.img_extension}', dpi=400, bbox_inches='tight', pad_inches=0.0) 
 
-    with open(args.output_path + args.log_file, 'a') as f:
-        f.write(f"\n\nDone.")
+        with open(args.output_path + args.log_file, 'a') as f:
+            f.write(f"\n\nDone.")
 
-
+            
