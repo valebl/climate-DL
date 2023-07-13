326089aa1e9ef4593d99c8bdf884e59b6f4232a0
valebl <kiwi5.vb@gmail.com>
2023-07-13 12:34:32 +0200

    add utils prediction

---
+++ preprocessing/utils_preprocessing.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+
+
+def create_zones(zones_file='/m100_work/ICT23_ESP_C/vblasone/precipitation-maps/Italia.txt'):
+    zones = []
+    with open(zones_file) as f:
+        lines = f.read()
+        for zone in lines.split(';'):
+            zones.append(zone)
+    for i in range(len(zones)):
+        zones[i] = zones[i].split('\n')
+        for j in range(len(zones[i])):
+            zones[i][j] = zones[i][j].split(',')
+        if [''] in zones[i]:
+            zones[i].remove([''])
+    for i in range(len(zones)):
+        for j in range(len(zones[i])):
+            if '' in zones[i][j]:
+                zones[i][j].remove('')
+            if zones[i][j] == []:
+                del zones[i][j]
+                continue
+            for k in range(len(zones[i][j])):
+                zones[i][j][k] = float(zones[i][j][k])
+    return zones
+
+
+def plot_italy(zones, ax, color='k', alpha_fill=0.1, linewidth=1):                                                                                                                                              
+    j = 0                                                                                                                                                                                                      
+    for zone in zones:       
+        x_zone = [zone[i][0] for i in range(len(zone)) if i > 0]
+        y_zone = [zone[i][1] for i in range(len(zone)) if i > 0]
+        ax.fill(x_zone, y_zone, color, alpha=alpha_fill)
+        ax.plot(x_zone, y_zone, color, alpha=1, linewidth=linewidth)
+        j += 1
+
