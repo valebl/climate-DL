b9156be123c79c8b535d89ae15a7575cea51b39a
valebl <kiwi5.vb@gmail.com>
2023-08-30 15:46:50 +0200

    small fix

--- preprocessing/slice_and_merge_era5.sh
+++ preprocessing/slice_and_merge_era5.sh
@@ -34,6 +34,10 @@ cd ${OUTPUT_PATH}
 ## merge all the sliced files into a single file for all the time span considered
 for v in 'q' 't' 'u' 'v' 'z' ; do
 	cdo -O -f nc4 -z zip -L -b F32 mergetime "${PREFIX}${v}_2001.nc" "${PREFIX}${v}_2002.nc" "${PREFIX}${v}_2003.nc" "${PREFIX}${v}_2004.nc" "${PREFIX}${v}_2005.nc" "${PREFIX}${v}_2006.nc" "${PREFIX}${v}_2007.nc" "${PREFIX}${v}_2008.nc" "${PREFIX}${v}_2009.nc" "${PREFIX}${v}_2010.nc" "${PREFIX}${v}_2011.nc" "${PREFIX}${v}_2012.nc" "${PREFIX}${v}_2013.nc" "${PREFIX}${v}_2014.nc" "${PREFIX}${v}_2015.nc" "${PREFIX}${v}_2016.nc" "${PREFIX}${v}.nc"
-	rm "${PREFIX}${v}_*"
+	## remove temporary files no longer usefule after merging
+	files=$(ls ${PREFIX}${v}_*.nc)
+	for file in $files ; do
+		rm file
+	done
 done
 
