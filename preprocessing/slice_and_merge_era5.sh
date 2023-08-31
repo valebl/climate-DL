4ab44f576a4ae2b9ab601f1681414b4a0b9a7dc4
valebl <kiwi5.vb@gmail.com>
2023-08-31 10:56:09 +0200

    fix slice and merge era5

--- preprocessing/slice_and_merge_era5.sh
+++ preprocessing/slice_and_merge_era5.sh
@@ -29,10 +29,12 @@ echo $lat_max_era5
 
 #echo "Done"
 
-#cd ${OUTPUT_PATH}
+cdo sellonlatbox,$lon_min_era5,$lon_max_era5,$lat_min_era5,$lat_max_era5 "${INPUT_PATH_PHASE_1A}q_2001.nc" "${OUTPUT_PATH}sliced_q_2001.nc"
+
+cd ${OUTPUT_PATH}
 
 ## merge all the sliced files into a single file for all the time span considered
-#for v in 'q' 't' 'u' 'v' 'z' ; do
+for v in 'q' 't' 'u' 'v' 'z' ; do
 	cdo -O -f nc4 -z zip -L -b F32 mergetime "${PREFIX}${v}_2001.nc" "${PREFIX}${v}_2002.nc" "${PREFIX}${v}_2003.nc" "${PREFIX}${v}_2004.nc" "${PREFIX}${v}_2005.nc" "${PREFIX}${v}_2006.nc" "${PREFIX}${v}_2007.nc" "${PREFIX}${v}_2008.nc" "${PREFIX}${v}_2009.nc" "${PREFIX}${v}_2010.nc" "${PREFIX}${v}_2011.nc" "${PREFIX}${v}_2012.nc" "${PREFIX}${v}_2013.nc" "${PREFIX}${v}_2014.nc" "${PREFIX}${v}_2015.nc" "${PREFIX}${v}_2016.nc" "${PREFIX}${v}.nc"
 	## remove temporary files no longer usefule after merging
 	files=$(ls ${PREFIX}${v}_*.nc)
