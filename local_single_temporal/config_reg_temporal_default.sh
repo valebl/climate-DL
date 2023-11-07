ac2ec0f96c5b712488546a20c687e4303117997c
valebl <kiwi5.vb@gmail.com>
2023-11-07 14:58:36 +0100

    update run rutine and config default files

--- local_single_temporal/config_reg_temporal_default.sh
+++ local_single_temporal/config_reg_temporal_default.sh
@@ -5,9 +5,9 @@
 #------------------#
 TIME="24:00:00"
 MEM="0"
-JOB_NAME="cl_temp"
+JOB_NAME="reg_temp"
 MAIL="vblasone@ictp.it"
-LOG_PATH="/leonardo_work/ICT23_ESP_0/vblasone/climate-DL/local_single_temporal/cl_fvg_temporal/"
+LOG_PATH="/leonardo_work/ICT23_ESP_0/vblasone/climate-DL/local_single_temporal/reg_fvg_temporal/"
 
 #--------------------------------------------#
 # PATHS (conda env, main, accelerate config) #
@@ -21,32 +21,33 @@ ACCELERATE_CONFIG_PATH="/leonardo/home/userexternal/vblasone/.cache/huggingface/
 # INPUT/OUTPUT FILES PARAMETERS #
 #-------------------------------#
 INPUT_PATH="/leonardo_work/ICT23_ESP_0/SHARED/preprocessed/fvg_temporal/"
-OUTPUT_PATH="/leonardo_work/ICT23_ESP_0/vblasone/climate-DL/local_single_temporal/cl_fvg_temporal/"
+OUTPUT_PATH="/leonardo_work/ICT23_ESP_0/vblasone/climate-DL/local_single_temporal/reg_fvg_temporal/"
 INPUT_FILE="input_standard.pkl"
-IDX_FILE="idx_train_cl.pkl"
+IDX_FILE="idx_train_reg.pkl"
 LOG_FILE="log.txt"
-TARGET_FILE="target_train_cl.pkl"
-MASK_TARGET_FILE="mask_train_cl.pkl"
+TARGET_FILE="target_train_reg.pkl"
+MASK_TARGET_FILE="mask_train_reg.pkl"
 GRAPH_FILE="G_train.pkl"
 SUBGRAPHS_FILE="subgraphs.pkl"
 OUT_CHECKPOINT_FILE="checkpoint.pth"
 OUT_LOSS_FILE="loss.csv"
 IDX_TIME_FILE="idx_time_train.pkl"
+WEIGHTS_FILE="weights_reg_train.pkl"
 
 #---------------------#
 # TRAINING PARAMETERS #
 #---------------------#
 PCT_TRAINING=1
-EPOCHS=20
+EPOCHS=50
 BATCH_SIZE=32
 LR_STEP_SIZE=25
 LR=0.0001
 WEIGHT_DECAY=0.0
-MODEL_NAME="Classifier_temporal"
-LOSS_FN="sigmoid_focal_loss"
-MODEL_TYPE="cl"
+MODEL_NAME="Regressor_temporal"
+LOSS_FN="weighted_mse_loss"
+MODEL_TYPE="reg"
 CHECKPOINT_FILE=None #"/leonardo_work/ICT23_ESP_0/vblasone/climate-DL/data_fvg_preprocessed/checkpoint_ae_e3.pth"
-WANDB_PROJECT_NAME="Classifier-temporal-fvg-leo"
+WANDB_PROJECT_NAME="Regressor-temporal-fvg-leo"
 
 #----------------------------------------#
 # COORDINATES OF CONSIDERED SPATIAL AREA #
@@ -101,6 +102,5 @@ fi
 if [ -z ${WEIGHTS_FILE} ]; then
 	WEIGHTS_FILE=""
 else
-	WEIGHTS_FILE="--weights_file=${WEIGHTS_FILE}"
+	WEIGHTS_FILE="--weights_file='${WEIGHTS_FILE}'"
 fi
-
--- local_single_temporal/run_temporal.sh
+++ local_single_temporal/run_temporal.sh
@@ -2,19 +2,35 @@
 source $1
 mkdir -p ${LOG_PATH}
 
+sbatch << EOT
+#!/bin/bash
+#SBATCH -A ict23_esp_0
+#SBATCH -p boost_usr_prod
+#SBATCH --time=${TIME}       # format: HH:MM:SS
+#SBATCH -N 1                  # 1 node
+#SBATCH --mem=${MEM}
+#SBATCH --ntasks-per-node=32 # out of 128
+#SBATCH --gres=gpu:4       # 1 gpus per node out of 4
+#SBATCH --job-name=${JOB_NAME}
+#SBATCH --mail-type=FAIL,END
+# SBATCH --mail-user=${MAIL}
+#SBATCH -o ${LOG_PATH}/run.out
+#SBATCH -e ${LOG_PATH}/run.err
+
+module purge
+module load --auto profile/deeplrn
+module load gcc
+module load cuda/11.8
+
 source ${SOURCE_PATH}
 
-#source ~/miniconda3/etc/profile.d/conda.sh
+#source ~/anaconda/etc/profile.d/conda.sh
 conda activate ${ENV_PATH}
 
 cd ${MAIN_PATH}
 
-if [ -z ${WEIGHTS_FILE} ]; then
-	WEIGHTS=""
-else
-	WEIGHTS="--weights_file=${WEIGHTS_FILE}"
-fi
-
 ## training
-accelerate launch --config_file ${ACCELERATE_CONFIG_PATH} main.py --input_path=${INPUT_PATH} --output_path=${OUTPUT_PATH} --input_file=${INPUT_FILE} --idx_file=${IDX_FILE} --log_file=${LOG_FILE} --target_file=${TARGET_FILE} --mask_target_file=${MASK_TARGET_FILE} --graph_file=${GRAPH_FILE} --subgraphs_file=${SUBGRAPHS_FILE} --out_checkpoint_file=${OUT_CHECKPOINT_FILE} --out_loss_file=${OUT_LOSS_FILE} --pct_trainset=${PCT_TRAINING} --epochs=${EPOCHS} --batch_size=${BATCH_SIZE} --step_size=${LR_STEP_SIZE} --lr=${LR} --weight_decay=${WEIGHT_DECAY} --model_name=${MODEL_NAME} --loss_fn=${LOSS_FN} --model_type=${MODEL_TYPE} --use_accelerate --load_checkpoint --no-test_model --no-ctd_training --fine_tuning --checkpoint_file=${CHECKPOINT_FILE} --wandb_project_name=${WANDB_PROJECT_NAME} --lon_min=${LON_MIN} --lon_max=${LON_MAX} --lat_min=${LAT_MIN} --lat_max=${LAT_MAX} ${USE_ACCELERATE} ${LOAD_CHECKPOINT} ${CTD_TRAINING} ${FINE_TUNING} ${WEIGHTS}
+accelerate launch --config_file ${ACCELERATE_CONFIG_PATH} main.py --input_path=${INPUT_PATH} --output_path=${OUTPUT_PATH} --input_file=${INPUT_FILE} --idx_file=${IDX_FILE} --log_file=${LOG_FILE} --target_file=${TARGET_FILE} --mask_target_file=${MASK_TARGET_FILE} --graph_file=${GRAPH_FILE} --subgraphs_file=${SUBGRAPHS_FILE} --out_checkpoint_file=${OUT_CHECKPOINT_FILE} --out_loss_file=${OUT_LOSS_FILE} --pct_trainset=${PCT_TRAINING} --epochs=${EPOCHS} --batch_size=${BATCH_SIZE} --step_size=${LR_STEP_SIZE} --lr=${LR} --weight_decay=${WEIGHT_DECAY} --model_name=${MODEL_NAME} --loss_fn=${LOSS_FN} --model_type=${MODEL_TYPE} --use_accelerate --load_checkpoint --no-test_model --no-ctd_training --fine_tuning --checkpoint_file=${CHECKPOINT_FILE} --wandb_project_name=${WANDB_PROJECT_NAME} --lon_min=${LON_MIN} --lon_max=${LON_MAX} --lat_min=${LAT_MIN} --lat_max=${LAT_MAX} ${USE_ACCELERATE} ${LOAD_CHECKPOINT} ${CTD_TRAINING} ${FINE_TUNING} ${WEIGHTS_FILE}
+EOT
+
 
